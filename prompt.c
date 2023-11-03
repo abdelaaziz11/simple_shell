57a5c191cba1b5da4325db0400d38f2d804d9aed
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-11-03 15:01:18 -0700

    prompt

--- prompt.c
+++ prompt.c
@@ -1,32 +1,30 @@
-#define _GNU_SOURCE
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - getline functoin
+ * @ac: arg
+ * @av: arg string
+ * Return: number of arg
+ */
+
+int main(int ac, char **av)
 {
-	FILE *stream;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
+	(void)av;
+	(void)ac;
+    
+	char *buffer;
+	size_t size_buffer = 0;
+	int n_char = 0;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	stream = fopen(argv[1], "r");
-	if (stream == NULL)
-	{
-		perror("fopen");
-		exit(EXIT_FAILURE);
-	}
-	while ((nread = getline(&line, &len, stream)) != -1)
-	{
-		printf("Retrieved line of length %zu:\n", nread);
-		fwrite(line, nread, 1, stdout);
-	}
-
-	free(line);
-	fclose(stream);
-	exit(EXIT_SUCCESS);
+	write(1, "$ ", 2);
+	n_char = getline(&buffer, &size_buffer, stdin);
+	if (!n_char)
+	perror("getline");
+	
+	printf("%s", buffer);
+	return (0);
 }
